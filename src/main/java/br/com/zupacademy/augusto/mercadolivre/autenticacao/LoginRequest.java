88516e22ac0641f350dbf99c2c8f86db9315235b
Augusto Neto <augusto.neto@zup.com.br>
2021-04-12 16:17:04 -0300

    remocao de getters e setters desnecessarios

--- src/main/java/br/com/zupacademy/augusto/mercadolivre/autenticacao/LoginRequest.java
+++ src/main/java/br/com/zupacademy/augusto/mercadolivre/autenticacao/LoginRequest.java
@@ -5,19 +5,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 public class LoginRequest {
 	private String email;
 	private String senha;
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+
 	public UsernamePasswordAuthenticationToken converter() {
 		return new UsernamePasswordAuthenticationToken(email, senha);
 	}
